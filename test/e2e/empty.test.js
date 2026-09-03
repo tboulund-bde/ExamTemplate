fe6681690d771c298f2eb221f52d0597a7cb54d1
tboulund <tbmh@easv.dk>
2026-09-03 14:18:12 +0200

    Empty test

---
+++ test/e2e/empty.test.js
@@ -0,0 +1,6 @@
+// Leave this file as it is and create new files in this folder for you E2E tests.
+fixture('Placeholder');
+
+test('Placeholder test', async t => {
+    await t.expect(true).ok();
+});
